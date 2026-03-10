26146d6fc14cdbb3b9086350a1656a753c087544
Akash <raut.akash0108@gmail.com>
2026-03-10 13:00:03 +0530

    Revert all Netlify functions changes: remove functions directory, revert thunks to direct API calls, update netlify.toml

--- src/features/banner/redux/bannerThunks.js
+++ src/features/banner/redux/bannerThunks.js
@@ -7,37 +7,26 @@ function fetchbannerMovies(){
     const IMAGE_BASE_URL = import.meta.env.VITE_IMAGE_BASE_URL;
 
     return async (dispatch) => {
-      //const url = `${BASE_URL}trending/movie/day?api_key=${API_KEY}`;
-        const url = "/.netlify/functions/movies";
-      dispatch(setLoader());
-      try {
-        const response = await axios.get(url);
-        let results = response?.data?.results || [];
-        // if serverless endpoint failed or returned nothing, fall back to client-side fetch
-        if (results.length === 0) {
-          const direct = await axios.get(
-            `${BASE_URL}trending/movie/day?api_key=${API_KEY}`
-          );
-          results = direct?.data?.results || [];
-        }
-
-        const top5movie = results.slice(0, 5);
-        dispatch(
-          setTop5Movies(
-            top5movie.map((movie) => {
-              let cleaned_Title = movie?.title.replace(/“|”/g, '');
-              return {
-                title: cleaned_Title,
-                bannerImage: `${IMAGE_BASE_URL}${movie?.backdrop_path}`,
-              };
-            }),
-          ),
-        );
-      } catch (error) {
-        dispatch(setErr(error));
-      } finally {
-        dispatch(stopLoaderB());
-      }
+      const url = `${BASE_URL}trending/movie/day?api_key=${API_KEY}`;
+      dispatch(setLoader())
+      axios.get(url).then((response) => {
+       const top5movie = response?.data?.results?.slice(0, 5);
+       dispatch( setTop5Movies(
+          top5movie.map((movie) => {
+            let cleaned_Title = movie?.title.replace(/“|”/g, '');
+            return {
+              title: cleaned_Title,
+              bannerImage: `${IMAGE_BASE_URL}${movie?.backdrop_path}`,
+            };
+          }),
+        ));
+      })
+     .catch ((error) => {
+       dispatch(setErr(error));
+      })
+    .finally(() => {
+      dispatch(stopLoaderB());
+    });
   }
 }
 
--- src/features/movies/redux/moviesThunks.js
+++ src/features/movies/redux/moviesThunks.js
@@ -8,8 +8,7 @@ function fetchTrendingMovies(pageNo){
     const BASE_URL = import.meta.env.VITE_BASE_URL;
     return async (dispatch) => {
       dispatch(setLoader())  
-    //const url = `${BASE_URL}discover/movie?api_key=${API_KEY}&page=${pageNo}`;
-      const url = `/.netlify/functions/movies2?page=${pageNo}`;
+    const url = `${BASE_URL}discover/movie?api_key=${API_KEY}&page=${pageNo}`;
     axios
       .get(url)
       .then((response) => {
